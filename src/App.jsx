3e31ded8b0aa8f55a4216ee627d49aef28c22cbb
DenisPepper <thech@yandex.ru>
2023-07-14 09:51:09 +0300

    Выполнит npm create vite@latest .

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+/*import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'*/
+
+export const App = () => {
+  return (
+    <div>Hello!</div>
+  )
+}
