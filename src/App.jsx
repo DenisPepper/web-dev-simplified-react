a4d37e841456995a6cec4a9d98a23a3274dcdc79
DenisPepper <thech@yandex.ru>
2024-06-11 18:49:26 +0300

    Добавит пример пользовательских хуков с useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 //import './App.css';
+import { ThemeToggler } from './exercises/custom-hooks/theme-toggler';
 import { UserCard } from './exercises/user-card/user-card';
 import user from './exercises/user-card/user.json';
 
 export function App() {
   return (
-    <UserCard
-      name={user.name}
-      age={user.age}
-      phoneNumber={user.phoneNumber}
-      address={user.address}
-    />
+    <>
+      <UserCard
+        name={user.name}
+        age={user.age}
+        phoneNumber={user.phoneNumber}
+        address={user.address}
+      />
+      <ThemeToggler />
+    </>
   );
 }
---
+++ src/exercises/custom-hooks/theme-toggler.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react';
+import './theme-toggler.css';
+
+export function ThemeToggler() {
+  const [isDark, toggleThemeMode] = useThemeToggler({ isDark: false });
+  const inputValue = useInputValue('');
+
+  return (
+    <div
+      className='theme-toggler'
+      style={{
+        background: isDark ? '#333' : 'white',
+        color: isDark ? 'white' : '#333',
+      }}
+    >
+      <label>
+        Name:
+        <input {...inputValue} />
+      </label>
+      <button type='button' onClick={toggleThemeMode}>
+        Toggle theme
+      </button>
+    </div>
+  );
+}
+
+const useThemeToggler = ({ isDark }) => {
+  const [isDarkTheme, setTheme] = useState(isDark);
+
+  const toggle = () => setTheme((isDark) => !isDark);
+
+  return [isDarkTheme, toggle];
+};
+
+const useInputValue = (initialValue) => {
+  const [value, setValue] = useState(initialValue);
+
+  return {
+    value,
+    onChange: (evt) => {
+      setValue(evt.target.value);
+    },
+  };
+};
