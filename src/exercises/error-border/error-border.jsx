a68247a2d2b5186e98a6a333828169d64c245775
DenisPepper <thech@yandex.ru>
2024-06-16 20:28:29 +0300

    Добавит пример границы ошибки

---
+++ src/exercises/error-border/error-border.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export class ErrorBorder extends React.Component {
+  state = { hasError: false };
+
+  static getDerivedStateFromError(error) {
+    return { hasError: true };
+  }
+
+  render() {
+    if (this.state.hasError) return this.props.fallback;
+    return this.props.children;
+  }
+}
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 //import { RouterProvider } from 'react-router-dom';
 //import { router } from './router.jsx';
 import { App } from './App.jsx';
+import { ErrorBorder } from './exercises/error-border/error-border.jsx';
 //import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/*<RouterProvider router={router} /> */}
-    <App />
+    <ErrorBorder fallback={<h1>Error border fallback!</h1>}>
+      <App />
+    </ErrorBorder>
   </React.StrictMode>
 );
