65e84ca4ffbe9c84b9743377de5420771020b917
DenisPepper <thech@yandex.ru>
2024-06-16 20:30:17 +0300

    Добавит пример границы ошибки для отдельного компонента

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
+import { ErrorBorder } from './exercises/error-border/error-border';
 import { SomeForm } from './exercises/forward-ref/some-form';
 
 export function App() {
-  return <SomeForm />;
+  return (
+    <ErrorBorder fallback={<h1>Error border fallback!</h1>}>
+      <SomeForm />
+    </ErrorBorder>
+  );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,11 @@ import ReactDOM from 'react-dom/client';
 //import { RouterProvider } from 'react-router-dom';
 //import { router } from './router.jsx';
 import { App } from './App.jsx';
-import { ErrorBorder } from './exercises/error-border/error-border.jsx';
 //import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/*<RouterProvider router={router} /> */}
-    <ErrorBorder fallback={<h1>Error border fallback!</h1>}>
-      <App />
-    </ErrorBorder>
+    <App />
   </React.StrictMode>
 );
