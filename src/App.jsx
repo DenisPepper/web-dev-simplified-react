2d343e23b4686b10f16d741d8eef172b6ec8ba06
DenisPepper <thech@yandex.ru>
2024-06-16 08:28:25 +0300

    Добавит пример использования глобальных переменных

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
-import { Parent } from './exercises/use-context/parent';
-
 export function App() {
-  return <Parent />;
+  return (
+    <>
+      <div style={{ backgroundColor: 'green' }}>
+        {import.meta.env.VITE_CONST}
+      </div>
+      <br />
+      <div style={{ backgroundColor: 'red' }}>
+        <span>переменная окружения SERVER будет не доступна на клиенте: </span>
+        {import.meta.env.SERVER}
+      </div>
+    </>
+  );
 }
